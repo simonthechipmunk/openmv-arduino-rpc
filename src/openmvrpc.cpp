a63c38291734a51925b0156b27d4d89b840158f6
Theolyn <simonthechipmunk.tcp@googlemail.com>
2026-03-27 19:59:06 +0100

    Remove another I2C init

--- src/openmvrpc.cpp
+++ src/openmvrpc.cpp
@@ -828,8 +828,6 @@ bool rpc_i2c##name##_master::put_bytes(uint8_t *data, size_t size, unsigned long
 \
     (void) timeout; \
     bool ok = true; \
-    port.begin(); \
-    port.setClock(__rate); \
 \
     for (size_t i = 0; i < size; i += 32) { \
         size_t size_remaining = size - i; \
